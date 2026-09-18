6b51dff78e024382f3e748f04e066019ca4cf938
Rui Campos <ruigabriel2@ua.pt>
2026-09-18 16:02:47 +0100

    Final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
         >
           <img
             className="footer-brand-logo"
-            src="/logos/logo-horizontal-cor.png"
+            src="/logos/PINHO.svg"
             alt="Pinho Arquitetura"
           />
         </Link>
